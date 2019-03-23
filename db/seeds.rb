20c996ace787a7254f49c2cc9854daaf3db66e12
daisuke <dada.00131@google.com>
2019-03-23 21:15:34 +0900

    h

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(user_name: 'aman', email: 'aaa@aaa.com', password: 'aaaaaa')
+User.create(user_name: 'aman', email: 'aaa@aaa', password: 'aaaaaa')
