5160eb3bac4d66561eb76227473f8bf94e0b1a6a
daisuke <dada.00131@google.com>
2019-04-20 16:47:37 +0900

    scroll調整

--- app/assets/javascripts/chat-room-hedder-fixed.js
+++ app/assets/javascripts/chat-room-hedder-fixed.js
@@ -1,6 +1,6 @@
 $(function() {
 	$(window).on('load scroll', function(){
-		if ($(window).scrollTop() > 300) {
+		if ($(window).scrollTop() > 50) {
 			$('.chat-room-hedder').addClass('fixed');
 		} else {
 			$('.chat-room-hedder').removeClass('fixed');
